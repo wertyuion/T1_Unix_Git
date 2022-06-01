f9a6f9a9d41d9e03aa7f1c87969dca8f49418917
wertyuion <natasha.deleg@est.ikiam.edu.ec>
2022-05-31 23:09:01 -0500

    Natasha Deleg sube el archivo netsize.sh que resulve el ejercicio 1 de la tarea 1

---
+++ netsize.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+echo "Nombre de archivo:../datos/Saavedra2013/n1.txt"
+echo "Numero de filas " ; cat n1,txt | wc-1 ; echo "Numero de columnas " ; head -n 1 n1.txt | tr -d '\n' | wc-c ; echo "Fin script"
+
+
+#Resultados
+#Número de filas: 97
+#Número de columas: 80
